2aaa925bd252af97cbfab175e02478e7e2940163
madaosoft <madao.soft@gmail.com>
2017-07-26 16:57:24 -0300

    add new patterns
    Added new patters.

--- src/life.js
+++ src/life.js
@@ -3,9 +3,9 @@ var rowSize = 0; //board.rows.length;
 var columnSize = 0; //board.rows[0].cells.length;
 var generation = 0;
 var running = false;
-var arrayCurrent;
-var arrayNew;
-             
+var arrayCurrent = [];
+var arrayNew = [];
+
 var patterns = {none:"none",
                 block:[[1,1],
                        [1,1]],
@@ -23,13 +23,23 @@ var patterns = {none:"none",
                       [1,0,0,1],
                       [1,0,0,1],
                       [0,1,0,0]],
-                beacon:}
+                beacon:[[1,1,0,0],
+                        [1,1,0,0],
+                        [0,0,1,1],
+                        [0,0,1,1]],
+                glider:[[0,0,1],
+                        [1,0,1],
+                        [0,1,1]],
+                lwss:[[1,0,0,1,0],
+                      [0,0,0,0,1],
+                      [1,0,0,0,1],
+                      [0,1,1,1,1]]}
 
 // var isDragging = false;
 // $("td")
 // .mousedown(function() {
     // isDragging = true;
-    // toggleState(arrayCurrent, this.parentNode.rowIndex, this.cellIndex);    
+    // toggleState(arrayCurrent, this.parentNode.rowIndex, this.cellIndex);
 // })
 // .mousemove(function() {
     // if(isDragging)
@@ -38,12 +48,11 @@ var patterns = {none:"none",
 // .mouseup(function() {
     // isDragging = false;
 // });
+// function pauseSimulation(){
+    // running = false;
+// }
 
-function getId(element) {
-    alert("row" + element.parentNode.rowIndex + 
-    " - column" + element.cellIndex);
-}
-
+// Toggle state of board's cell
 function toggleState(array, row, column){
     if(array[row][column] == 1)
         array[row][column] = 0;
@@ -52,8 +61,9 @@ function toggleState(array, row, column){
     updateTable($('#board')[0].firstChild, array);
 }
 
+// Erase board colors and reset current array
 function clearBoard(){
-    for(var i = 0; i < rowSize; i++){           
+    for(var i = 0; i < rowSize; i++){
         for(var j = 0; j < columnSize; j++){
             arrayCurrent[i][j] = 0;
         }
@@ -62,11 +72,13 @@ function clearBoard(){
     updateTable($('#board')[0].firstChild, arrayCurrent);
 }
 
+// Set generation(=iteration) value and message
 function setGeneration(g){
     generation = g;
     document.getElementById("generation").innerHTML = "generation: " + g;
 }
 
+// Create an array with rowSize x columnSize
 function createArray(){
     var array = [];
     for(var i = 0; i < rowSize; i++){
@@ -78,23 +90,32 @@ function createArray(){
     return array;
 }
 
-function pauseSimulation(){
-    running = false;
-}
-
+// Execute an iteration
+// A cell with value 0 is dead and 1 is alive
 function run(){
+    // Sum of neighbours of a cell + cell itself
     var sum = 0;
-    for(var i = 0; i < rowSize; i++){           
+
+    for(var i = 0; i < rowSize; i++){
         for(var j = 0; j < columnSize; j++){
             sum = sumNeighbours(arrayCurrent, i, j);
+
+            // Live cell
             if(arrayCurrent[i][j] == 1){
+                // Any live cell with fewer than two live neighbours dies
+                // Any live cell with more than three live neighbours dies
                 if(sum < 3 || sum > 4){
                     arrayNew[i][j] = 0;
                 }
+                // Any live cell with two or three live neighbours
+                // lives on to the next generation
                 else
                     arrayNew[i][j] = 1;
             }
-            else { // dead cell
+            // Dead cell
+            else {
+                // Any dead cell with exactly three live neighbours
+                // becomes a live cell
                 if(sum == 3)
                     arrayNew[i][j] = 1;
                 else
@@ -102,8 +123,11 @@ function run(){
             }
         }
     }
+
     updateTable($('#board')[0].firstChild, arrayNew);
-    for(var i = 0; i < rowSize; i++){           
+
+    // Copy the new array to the current array for next iteration
+    for(var i = 0; i < rowSize; i++){
         for(var j = 0; j < columnSize; j++){
             arrayCurrent[i][j] = arrayNew[i][j];
         }
@@ -111,40 +135,43 @@ function run(){
     setGeneration(++generation);
 }
 
+// Sum of neighbours of a cell and cell itself
 function sumNeighbours(array, x, y){
     var sum = 0;
-    var length = array.length;
+    var row = array.length;
+    var column = array[0].length;
     var newX = 0;
     var newY = 0;
-    
-    for(var i = -1; i < 2; i++){           
+
+    for(var i = -1; i < 2; i++){
         for(var j = -1; j < 2; j++){
             newX = x + i;
             newY = y + j;
-            
-            // Infinite board representation
+
+            // Infinite board representation (like global map)
             if(newX < 0)
-                newX = newX + length;
+                newX = newX + row;
             if(newY < 0)
-                newY = newY + length;
-            
-            newX = newX % length;
-            newY = newY % length;
-            
+                newY = newY + column;
+
+            newX = newX % row;
+            newY = newY % column;
+
             sum = sum + array[newX][newY];
         }
     }
     return sum;
 }
 
+// Get a number between min and max
 function getRndInteger(min, max) {
-    return Math.floor( Math.random() * (max - min + 1) ) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Update the colors of board's cells
 function updateTable(table, array){
-    for(var i = 0; i < rowSize; i++){           
+    for(var i = 0; i < rowSize; i++){
         for(var j = 0; j < columnSize; j++){
-            // table.rows[i].cells[j].innerHTML = array[i][j];
             if(array[i][j] == 1){
                 table.rows[i].cells[j].style.backgroundColor = "green";
             }
@@ -155,20 +182,19 @@ function updateTable(table, array){
     }
 }
 
+// Create a html table
 function createTable(row, column, id){
+    var table = document.createElement('table');
+
     rowSize = row;
     columnSize = column;
     arrayCurrent = setPattern();
     arrayNew = createArray();
-    var table = document.createElement('table');
-    
-    for (var i = 0; i < row; i++){
-        var tr = document.createElement('tr');   
-        for (var j = 0; j < column; j++){
-            var td = document.createElement('td');
-            // var txt = document.createTextNode('txt');
 
-            // td.appendChild(txt);
+    for (var i = 0; i < rowSize; i++){
+        var tr = document.createElement('tr');
+        for (var j = 0; j < columnSize; j++){
+            var td = document.createElement('td');
             tr.appendChild(td);
             table.appendChild(tr);
         }
@@ -176,48 +202,55 @@ function createTable(row, column, id){
     // document.body.appendChild(table);
     $(id).empty();
     $(id).append(table);
-    
+
+    // Add a click event listener to each cell
     $("td").click(function(e){
-        // getId(this);
         toggleState(arrayCurrent, this.parentNode.rowIndex, this.cellIndex);
         setGeneration(0);
     });
-    
+
     setGeneration(0);
+
+    // $('#board')[0].firstChild is html table #board
     updateTable($('#board')[0].firstChild, arrayCurrent);
 }
 
+// Set an initial pattern to be created
 function setPattern(){
     var p = [];
     var row = 0;
     var column = 0;
     var id = "";
-    
-    // switch($("input[name=pattern]:checked").val()){
-        // case "none":
-            // break;
-        // case "1":
-            // p = patterns.block;
-            // break;            
-    // }
-    
-    // console.log($("input[name=pattern]:checked").val());
+    var array = createArray();
+
+    // Get the pattern chosen(radio button)
     p = patterns[$("input[name=pattern]:checked").val()];
-    
+
+    // No initial pattern
     if(p != "none"){
         row = p.length;
-        column = p[0].length; 
+        column = p[0].length;
     }
-    
-    // console.log("row: " + row + " column: " + column);
-    
-    arrayCurrent = createArray();
-    for(var i = 0; i < row; i++){           
+
+    // Set the pattern
+    for(var i = 0; i < row; i++){
         for(var j = 0; j < column; j++){
-            arrayCurrent[i+1][j+1] = p[i][j];
+            array[i+1][j+1] = p[i][j];
         }
     }
-    return arrayCurrent;
+    return array;
+}
+
+// Print a array
+function printArray(array, id,i,j){
+    document.getElementById(id).innerHTML += i+","+j+"<br />";
+    for(var i = 0; i < rowSize; i++){
+        for(var j = 0; j < columnSize; j++){
+            document.getElementById(id).innerHTML += " " + array[i][j];
+        }
+        document.getElementById(id).innerHTML += "<br />"
+    }
+    document.getElementById(id).innerHTML += "<br />"
 }
 
 function main(){
@@ -226,6 +259,5 @@ function main(){
             run();
         }
     });
-    console.log($("input[name=pattern]:checked").val());
 }
 main();
